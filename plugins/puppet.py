a0c5209e21276411c459abd75ab841d245b0802f
Paul Lathrop <paul@getspectrum.io>
2021-03-19 19:12:06 -0400

    Don't double-implement quit.

--- plugins/puppet.py
+++ plugins/puppet.py
@@ -1,11 +1,6 @@
 import pinhook.plugin
 
 
-@pinhook.plugin.command('~quit', ops=True, ops_msg='This command can only be run by a bot op!')
-def quit_plugin(message):
-    message.bot.die("Killing myself, master {}!".format(message.nick))
-
-
 @pinhook.plugin.command('~say', ops=True, ops_msg='This command is reserved for bot ops!')
 def say_plugin(message):
     parts = message.arg.split(" ")
